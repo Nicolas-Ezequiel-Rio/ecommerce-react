5789057cc4d623c56137102a450ff632fbcab8ff
mleguizamongorosito <mleguizamongorosito@frba.utn.edu.ar>
2024-09-08 11:45:42 -0300

    refactor: #1 First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <p className='text-center text-red-500'>Hola Mundo!</p>
+  return (
+    <>
+      <div>
+        <p className='text-center text-red-500'>Hola Mundo!</p>
+      </div>
+    </>
+  )
 }
