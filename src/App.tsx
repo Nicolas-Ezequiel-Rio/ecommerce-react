7fab5da28165be0cd94d59a1dbfb6af1b20f6c8f
Nicolas-Ezequiel-Rio <nrio@frba.utn.edu.ar>
2024-09-09 17:34:06 -0300

    feat: #2 Signin/Signup
    Signin and Signup were added.
    Routes added to Main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import Signup from './signup/signup.tsx'
+import Signin from './signin/signin.tsx'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+
 export default function App() {
-  return <p className='text-center text-red-500'>Hola Mundo!</p>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <p>
+                <NavLink to={'/signin'}>Signin</NavLink> {' | '}
+                <NavLink to={'/signup'}>Signup</NavLink>
+              </p>
+            }
+          />
+          <Route path='signup' element={<Signup />} />
+          <Route path='signin' element={<Signin />} />
+          <Route path='*' element={<h1>404: Page not found.</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
