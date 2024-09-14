9c52402935c26918f84fb732da6c6236b8d45969
mleguizamongorosito <mleguizamongorosito@frba.utn.edu.ar>
2024-09-14 13:21:18 -0300

    feat: #1 not found component was added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Signup from './components/signup/signup.tsx'
 import Signin from './components/signin/signin.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NavbarComponent from './components/navbar/Navbar.tsx'
+import NotFound from './components/not-found/NotFound.tsx'
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
             <Route path='signup' element={<Signup />} />
             <Route path='signin' element={<Signin />} />
           </Route>
-          <Route path='*' element={<h1>404: Page not found.</h1>} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/not-found/NotFound.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <>
+      <figure className=' flex justify-center'>
+        <img
+          className='h-auto max-w-full rounded-lg content-center'
+          src='./src/assets/not-found.png'
+          alt='Not Found'
+        />
+      </figure>
+    </>
+  )
+}
