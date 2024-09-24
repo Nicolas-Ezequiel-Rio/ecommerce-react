269870863edc4f5991e3af76fe05f1208dad56af
Nicolas-Ezequiel-Rio <nrio@frba.utn.edu.ar>
2024-09-24 19:30:03 -0300

    feat: #8 navbar
    navbar was updated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -105,10 +105,10 @@ export default function NavbarComponent({
           <Navbar.Link href='/' active>
             Home
           </Navbar.Link>
-          <Navbar.Link href='#'>About</Navbar.Link>
-          <Navbar.Link href='#'>Services</Navbar.Link>
-          <Navbar.Link href='#'>Pricing</Navbar.Link>
+          <Navbar.Link href='#'>Shop</Navbar.Link>
+          <Navbar.Link href='#'>Cart</Navbar.Link>
           <Navbar.Link href='#'>Contact</Navbar.Link>
+          <Navbar.Link href='#'>About</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
     </>
